0e2f39e4ddc0c88b31b20195f9facacbdfc81fc7
Thomas Mangin <thomas.mangin@exa-networks.co.uk>
2009-06-14 07:00:13 +0000

    added parsing of update-source in peer-group

--- lib/network/parse/cisco.py
+++ lib/network/parse/cisco.py
@@ -12,19 +12,22 @@ class Cisco (object):
 	regex_ipv4 = '(25[0-5]|2[0-4]\d|1\d{2}|\d{2}|\d)\.(25[0-5]|2[0-4]\d|1\d{2}|\d{2}|\d)\.(25[0-5]|2[0-4]\d|1\d{2}|\d{2}|\d)\.(25[0-5]|2[0-4]\d|1\d{2}|\d{2}|\d)'
 	# taken from http://forums.dartware.com/viewtopic.php?t=452
 	regex_ipv6 = '((([0-9A-Fa-f]{1,4}:){7}(([0-9A-Fa-f]{1,4})|:))|(([0-9A-Fa-f]{1,4}:){6}(:|((25[0-5]|2[0-4]\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3})|(:[0-9A-Fa-f]{1,4})))|(([0-9A-Fa-f]{1,4}:){5}((:((25[0-5]|2[0-4]\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3})?)|((:[0-9A-Fa-f]{1,4}){1,2})))|(([0-9A-Fa-f]{1,4}:){4}(:[0-9A-Fa-f]{1,4}){0,1}((:((25[0-5]|2[0-4]\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3})?)|((:[0-9A-Fa-f]{1,4}){1,2})))|(([0-9A-Fa-f]{1,4}:){3}(:[0-9A-Fa-f]{1,4}){0,2}((:((25[0-5]|2[0-4]\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3})?)|((:[0-9A-Fa-f]{1,4}){1,2})))|(([0-9A-Fa-f]{1,4}:){2}(:[0-9A-Fa-f]{1,4}){0,3}((:((25[0-5]|2[0-4]\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3})?)|((:[0-9A-Fa-f]{1,4}){1,2})))|(([0-9A-Fa-f]{1,4}:)(:[0-9A-Fa-f]{1,4}){0,4}((:((25[0-5]|2[0-4]\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3})?)|((:[0-9A-Fa-f]{1,4}){1,2})))|(:(:[0-9A-Fa-f]{1,4}){0,5}((:((25[0-5]|2[0-4]\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3})?)|((:[0-9A-Fa-f]{1,4}){1,2})))|(((25[0-5]|2[0-4]\d|[01]?\d{1,2})(\.(25[0-5]|2[0-4]\d|[01]?\d{1,2})){3})))(%.+)?'
+	regex_interface = '(?P<interface>.*)\s*(?P<number>\d+(/\d+)?)'
 
 	expr_hostname = re.compile('^\s*hostname\s+(?P<router>.*)\s*$')
 
-	expr_interface = re.compile('^\s*interface\s+(.*)\s*$')
+	expr_interface = re.compile('^\s*interface\s+%s\s*$' % regex_interface)
 	expr_bgp_family = re.compile('^\s*address-family ipv(?P<family>4|6)\s*$')
 	expr_exit = re.compile('^\s*!\s*')
 
 	expr_ipv4 = re.compile('\s*ip\s+address\s+(?P<ip>%s)\s+(?P<netmask>%s)\s*' % (regex_ipv4,regex_ipv4))
 	expr_ipv6 = re.compile('\s*ipv6\s+address\s+(?P<ip>%s)\s*/\s*(?P<netmask>12[0-8]|1[01]\d|\d{2}|\d)\s*' % regex_ipv6)
 
-	expr_neighbor_remote_as = re.compile('\s*neighbor\s+(?P<neighbor>(?P<neighbor_v4>%s)|(?P<neighbor_v6>%s))\s+remote-as\s+(?P<asn>\d+)\s*' % (regex_ipv4,regex_ipv6))
+	expr_neighbor_remote_asn = re.compile('\s*neighbor\s+(?P<neighbor>(?P<neighbor_v4>%s)|(?P<neighbor_v6>%s))\s+remote-as\s+(?P<asn>\d+)\s*' % (regex_ipv4,regex_ipv6))
 
-	expr_group_remote_as = re.compile('\s*neighbor\s+(?P<peer_group>[a-zA-Z0-9\-]+)\s+remote-as\s+(?P<asn>\d+)\s*')
+	expr_group_remote_asn = re.compile('\s*neighbor\s+(?P<peer_group>[a-zA-Z0-9\-]+)\s+remote-as\s+(?P<asn>\d+)\s*')
+	expr_group_update_source = re.compile('\s*neighbor\s+(?P<peer_group>[a-zA-Z0-9\-]+)\s+update-source\s+%ss*' % regex_interface)
+
 	expr_peer_group = re.compile('\s*neighbor\s+(?P<peer_group>%s|%s)\s+peer-group\s+(?P<name>.*)\s*' % (regex_ipv4,regex_ipv6))
 
 	expr_any_neighbor = re.compile('\s*neighbor\s+(?P<neighbor>%s|%s)\s+.*' % (regex_ipv4,regex_ipv6))
@@ -54,13 +57,15 @@ class Cisco (object):
 		neighbors = {}
 		network = {}
 		group_asn = {}
+		group_interface = {}
 		peer_group = {}
-		v4_interface = {}
-		v6_interface = {}
+		interface_ipv4 = {}
+		interface_ipv6 = {}
+		interface_netmask = {}
 		v4_peer = {}
 		v6_peer = {}
 		peer_type = {}
-		remote = {}
+		remote_asn = {}
 		descr = {}
 		ignore = []
 		router='no-router-name'
@@ -68,12 +73,15 @@ class Cisco (object):
 		in_bgp = False
 		family_version = 0
 		family_cast = 'unicast'
+		last_interface = 'no-interface'
 
 		for line in stream:
 			line = line[:-1]
 			# we only care about the router bgp section and interface definition
 			if not in_interface and not in_bgp and not family_version:
-				if self.expr_interface.search(line):
+				match = self.expr_interface.search(line)
+				if match:
+					last_interface = match.group('interface')+match.group('number')
 					in_interface = True
 					continue
 				match = self.expr_router_bgp.search(line)
@@ -95,12 +103,14 @@ class Cisco (object):
 					continue
 				match = self.expr_ipv4.search(line)
 				if match:
-					v4_interface[match.group('ip')] = match.group('netmask')
+					interface_netmask[match.group('ip')] = match.group('netmask')
+					interface_ipv4[last_interface] = match.group('ip')
 					network[address.IPv4NetworkNetmask(match.group('ip'),match.group('netmask'))] = match.group('ip')
 					continue
 				match = self.expr_ipv6.search(line)
 				if match:
-					v6_interface[match.group('ip')] = match.group('netmask')
+					interface_netmask[match.group('ip')] = match.group('netmask')
+					interface_ipv6[last_interface] = match.group('ip')
 					continue
 				continue
 			
@@ -124,15 +134,19 @@ class Cisco (object):
 					ignore.append(match.group('neighbor'))
 					continue
 
-				match = self.expr_neighbor_remote_as.search(line)
+				match = self.expr_neighbor_remote_asn.search(line)
 				if match:
-					remote[match.group('neighbor')] = match.group('asn')
+					remote_asn[match.group('neighbor')] = match.group('asn')
 					continue
 				
-				# must follow expr_neighbor_remote_as for when it fails
-				match = self.expr_group_remote_as.search(line)
+				# must follow expr_neighbor_remote_asn for when it fails
+				match = self.expr_group_remote_asn.search(line)
 				if match:
 					group_asn[match.group('peer_group')] = match.group('asn')
+
+				match = self.expr_group_update_source.search(line)
+				if match:
+					group_interface[match.group('peer_group')] = match.group('interface')+match.group('number')
 
 				# must be before other peer group search
 				match = self.expr_peer_group.search(line)
@@ -220,25 +234,31 @@ class Cisco (object):
 				
 			macro,name,mail,announce = descr[dest]
 
-			if dest in remote:
-				asn = remote[dest]
+			if dest in remote_asn:
+				asn = remote_asn[dest]
 			elif group in group_asn:
 				asn = group_asn[group]
 			else:
 				if debug: print "no asn information for", dest
 				continue
 
-			# hard work to get it from the interface commiting the current work first ...
-			if address.isIPv4(dest):
-				src = '0.0.0.0'
-				for r in range(32,0,-1):
-					m = address.IPv4bitsToNetmask(r)
-					n = address.IPv4NetworkNetmask(dest,m)
-					if network.has_key(n):
-						src = network[n]
-						continue
-			else:
-				src = '::'
+			try:
+				if address.isIPv4(dest):
+					src = interface_ipv4[group_interface[peer_group[dest]]]
+				else:
+					src = interface_ipv6[group_interface[peer_group[dest]]]
+			except KeyError:
+				# hard work to get it from the interface commiting the current work first ...
+				if address.isIPv4(dest):
+					src = '0.0.0.0'
+					for r in range(32,0,-1):
+						m = address.IPv4bitsToNetmask(r)
+						n = address.IPv4NetworkNetmask(dest,m)
+						if network.has_key(n):
+							src = network[n]
+							continue
+				else:
+					src = '::'
 
 			if type == 'peer' and macro != '' and dest != '' and src != '':
 				if announce == '': announce = self.announced_set
